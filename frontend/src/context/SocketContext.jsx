e4850366ee06a038a84e27cf980693b575862abc
Treasure-king <107706054+Treasure-king@users.noreply.github.com>
2024-05-03 13:37:06 +0530

    Socket.io Updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = (props)=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:5000",{
+            const socket = io("https://mern-chat-app-cbjx.onrender.com",{
                 query:{
                     userId:authUser._id
                 }
