a9f148189f2b8513dedac3b2dbc9d3c3c10b2475
Treasure-king <107706054+Treasure-king@users.noreply.github.com>
2024-05-03 13:27:00 +0530

    connectmongoDB update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.routes.js'
 
-import connectTOMongoDB from "./db/connectToMongodb.js"
+import connectTOMongoDB from "./db/connectToMongoDB.js"
 import { app, server } from "./socket/socket.js"
 
 
